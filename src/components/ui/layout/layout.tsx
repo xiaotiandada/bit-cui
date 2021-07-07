be708daafbf469604c0281c7c8296df95216e078
xiaotiandada <istianlei@qq.com>
2021-07-08 01:47:28 +0800

    feat: layout

--- src/components/ui/layout/layout.tsx
+++ src/components/ui/layout/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styles from "./layout.module.scss";
 import { ListProps } from './index.d'
 
 export type LayoutProps = {
@@ -15,89 +15,26 @@ export function Layout({ list }: LayoutProps) {
 
   const Item: React.FC<AboutProps> = ({ cover, text, link }) => {
     return (
-      <StyledItem target='_blank' href={link} rel="noopener noreferrer">
-        <StyledItemCover>
-          <StyledItemImg src={cover} alt={text} />
-        </StyledItemCover>
-        <StyledItemText>{text}</StyledItemText>
-      </StyledItem>
+      <a target='_blank' href={link} rel="noopener noreferrer" className={styles.item}>
+        <div className={ styles.itemCover }>
+          <img className={ styles.itemImg } src={cover} alt={text} />
+        </div>
+        <p className={ styles.itemText }>{text}</p>
+      </a>
     );
   };
 
   return (
-    <StyledWrapper>
+    <div className={ styles.wrapper }>
       {list.map((i, idx) => (
-        <StyledBox key={idx}>
+        <div key={idx} className={styles.box}>
           <Item
             cover={i.cover}
             text={i.title}
             link={i.link}></Item>
-        </StyledBox>
+        </div>
       ))}
-    </StyledWrapper>
+    </div>
   );
 }
 
-const StyledWrapper = styled.div`
-  display: grid;
-  grid: repeat(2, 1fr) / repeat(4, 1fr);
-  grid-row-gap: 22px;
-  grid-column-gap: 24px;
-  margin-top: 48px;
-  @media screen and (max-width: 768px) {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-`;
-
-const StyledBox = styled.div`
-  width: 100%;
-  height: 100%;
-  /* background: red; */
-  &:nth-of-type(1) {
-    grid-row: 1 / 3;
-    grid-column: 1 / 3;
-  }
-  &:nth-of-type(2) {
-    grid-row: 1 / 3;
-    grid-column: 3 / 4;
-  }
-  &:nth-of-type(3),
-  &:nth-of-type(4) {
-  }
-`;
-
-const StyledItem = styled.a`
-  text-decoration: none;
-`;
-
-const StyledItemCover = styled.div`
-  width: 100%;
-  height: calc(100% - 38px);
-  overflow: hidden;
-  @media screen and (max-width: 576px) {
-    height: calc(100% - 28px);
-  }
-`
-
-const StyledItemImg = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`
-const StyledItemText = styled.p`
-  font-size: 18px;
-  font-weight: 500;
-  color: #333333;
-  margin: 0;
-  padding: 0 6px;
-  text-decoration: none;
-  height: 38px;
-  display: flex;
-  align-items: center;
-  @media screen and (max-width: 576px) {
-    font-size: 14px;
-    height: 28px;
-  }
-`
